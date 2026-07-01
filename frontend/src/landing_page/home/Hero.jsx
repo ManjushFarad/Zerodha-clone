f2a56f4be24ae33fbae870d5388daedba93c6d04
Manjush <manjushfarad@gmail.com>
2026-07-02 01:56:28 +0530

    added authentication

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Hero() {
     return ( 
@@ -7,7 +8,7 @@ function Hero() {
                <img src="/media/images/homeHero.png" alt='Hero Image' className='mb-5'/>
                 <h1 className='mt-2'>Invest in everything</h1>
                 <p className='mt-2'>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <button className='p-1 btn btn-primary fs-5 mt-2 mb-5' style={{width :"14%", margin : "0 auto"}}>Signup Now</button>
+                <Link to="/signup" className='p-1 btn btn-primary fs-5 mt-2 mb-5' style={{width :"14%", margin : "0 auto"}}>Signup Now</Link>
             </div>
         </div>
      );
--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Universe() {
     return ( 
@@ -105,12 +106,13 @@ function Universe() {
   </p>
 </div>
 
-<button
+<Link
+          to="/signup"
           className="p-1 btn btn-primary fs-5 mb-5"
           style={{ width: "15%", margin: "2rem auto" ,backgroundColor:"#387ED1"}}
         >
           Sign up for free
-        </button>
+        </Link>
 </div>
                 
             </div>
